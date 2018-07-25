55184bbc5fdfc9a69e6e2b63bf93193c01fbe5c3
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-07-25 14:57:19 +0300

    Player Movement to right.

---
+++ SalmonRun/Assets/Scripts/RunMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunMove : MonoBehaviour {
+
+    public float Speed;
+    Rigidbody2D PlayerRig;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerRig = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+}
