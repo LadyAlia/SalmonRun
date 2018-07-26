d097dc4b0d2a7f1dc6fad773a35ec7caa607001e
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-07-26 09:59:01 +0300

    Camera follow player on X Axis.

---
+++ SalmonRun/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+    }
+}
--- SalmonRun/Assets/Scripts/RunMove.cs
+++ SalmonRun/Assets/Scripts/RunMove.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class RunMove : MonoBehaviour {
 
+
     public float Speed;
+    public float JumpSpeed;
     Rigidbody2D PlayerRig;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         PlayerRig = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         transform.Translate(Vector3.right * Speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.J) || Input.touchCount > 0)
+        {
+            transform.Translate(Vector3.up * JumpSpeed * Time.deltaTime);
+        }
     }
 }
