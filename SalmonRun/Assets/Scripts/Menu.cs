c5863ccc06c39815aaf0281a3dc70c495417fb7e
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-07-27 13:31:08 +0300

    MenuScene Peliin.

---
+++ SalmonRun/Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public Canvas MenuCanvas;
+    public Canvas CreditsCanvas;
+  
+
+    // Use this for initialization
+    void Start ()
+    {
+        MenuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
+        CreditsCanvas = GameObject.Find("CreditsCanvas").GetComponent<Canvas>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("DevScene");
+    }
+
+    public void ToCredits()
+    {
+        MenuCanvas.enabled = false;
+        CreditsCanvas.enabled = true;
+    }
+
+    public void ToMenu()
+    {
+        MenuCanvas.enabled = true;
+        CreditsCanvas.enabled = false;
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
