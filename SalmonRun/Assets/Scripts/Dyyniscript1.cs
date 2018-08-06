d9283ec894519fbb5f6ed9f81ead9745e2de275d
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2018-08-06 12:14:38 +0300

    Music.

--- SalmonRun/Assets/Scripts/Dyyniscript1.cs
+++ SalmonRun/Assets/Scripts/Dyyniscript1.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Dyyniscript1 : MonoBehaviour {
 
     public float Speed;
-
+    public string musicOne;
     // Update is called once per frame
+
+    private void Start() {
+        Fabric.EventManager.Instance.PostEvent(musicOne);
+    }
 
     void Update () {
 
--- SalmonRun/Assets/Scripts/Menu.cs
+++ SalmonRun/Assets/Scripts/Menu.cs
@@ -7,13 +7,19 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour {
     public Canvas MenuCanvas;
     public Canvas CreditsCanvas;
+
+    public string bgmAudio;
+    public string stopAudio;
+
   
 
     // Use this for initialization
     void Start ()
     {
+        Fabric.EventManager.Instance.PostEvent(bgmAudio);
         MenuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
         CreditsCanvas = GameObject.Find("CreditsCanvas").GetComponent<Canvas>();
+        
 
     }
 
@@ -24,6 +30,8 @@ public class Menu : MonoBehaviour {
 
     public void PlayGame()
     {
+        Fabric.EventManager.Instance.PostEvent(stopAudio);
+
         SceneManager.LoadScene("TheGame");
     }
 
@@ -35,6 +43,7 @@ public class Menu : MonoBehaviour {
 
     public void ToMenu()
     {
+        
         SceneManager.LoadScene("MenuScene");
         MenuCanvas.enabled = true;
         CreditsCanvas.enabled = false;
